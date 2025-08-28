fd724e5cfa0b4d358aa73382023934f79437b000
Avinash Jadhav <avinashjadhav2468@gmail.com>
2025-08-28 09:19:21 +0530

    Initial commit

--- frontend/utils/Firebase.js
+++ frontend/utils/Firebase.js
@@ -2,11 +2,11 @@ import {getAuth, GoogleAuthProvider} from "firebase/auth"
 import { initializeApp } from "firebase/app";
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_APIKEY ,
-  authDomain: "loginonecart.firebaseapp.com",
-  projectId: "loginonecart",
-  storageBucket: "loginonecart.firebasestorage.app",
-  messagingSenderId: "242165258894",
-  appId: "1:242165258894:web:0155a2ced93e20073247df"
+  authDomain: "loginonecart-d1053.firebaseapp.com",
+  projectId: "loginonecart-d1053",
+  storageBucket: "loginonecart-d1053.firebasestorage.app",
+  messagingSenderId: "772914827007",
+  appId: "1:772914827007:web:066a5cb9293c58364806ca"
 };
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app)
